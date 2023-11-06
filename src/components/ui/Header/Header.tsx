ba8bc3a6fbc4935ca34385f42e83bac9fe35061c
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2023-11-06 18:47:14 +0200

    made simple header

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,5 +1,37 @@
+import './Header.scss';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <a className="header__logo" href="">
+        Logo
+      </a>
+      <nav>
+        <ul className="header__nav">
+          <li className="header__nav-item">
+            <a className="header__nav-item-link" href="">
+              About me
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <a className="header__nav-item-link" href="">
+              Skills
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <a className="header__nav-item-link" href="">
+              Portfolio
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <a className="header__nav-item-link" href="">
+              Expierence
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
