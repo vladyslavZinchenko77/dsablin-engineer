eb6afc69c3339755e38259a03af020e1d29350f9
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2023-11-06 22:50:27 +0200

    made hero section

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <Header />
+      {/* <Header /> */}
       <Hero />
       <Footer />
     </>
--- src/components/ui/Hero/Hero.tsx
+++ src/components/ui/Hero/Hero.tsx
@@ -1,5 +1,20 @@
+import panorama from '../../../assets/img/panorama-black-white.png';
+import './Hero.scss';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero">
+      <div className="hero__introduce">
+        <h1 className="hero__introduce-title">Hi, I am Dmytro Sublin</h1>
+        <h2 className="hero__introduce-subtitle">a construction engineer</h2>
+      </div>
+      <img
+        className="hero__avatar-img--panorama"
+        src={panorama}
+        alt="panorama"
+      />
+    </section>
+  );
 };
 
 export default Hero;
