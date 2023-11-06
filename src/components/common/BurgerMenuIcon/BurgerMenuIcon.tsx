6501c4431d55990c4eafc0316d6d07aeea745bf1
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2023-11-07 00:26:58 +0200

    made styles for header

---
+++ src/components/common/BurgerMenuIcon/BurgerMenuIcon.tsx
@@ -0,0 +1,13 @@
+import './BurgerMenuIcon.scss';
+
+const BurgerMenuIcon = () => {
+  return (
+    <div className="burger-icon">
+      <div className="burger-icon__firtst"></div>
+      <div className="burger-icon__second"></div>
+      <div className="burger-icon__third"></div>
+    </div>
+  );
+};
+
+export default BurgerMenuIcon;
