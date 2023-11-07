0db2b067281740ec90cd1e0f80812e83c2062a95
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2023-11-07 13:03:33 +0200

    made about section

---
+++ src/components/common/ButtonMain/ButtonMain.tsx
@@ -0,0 +1,11 @@
+import './ButtonMain.scss';
+
+const ButtonMain = () => {
+  return (
+    <a href="#" className="button-main">
+      <span></span>Eslam
+    </a>
+  );
+};
+
+export default ButtonMain;
