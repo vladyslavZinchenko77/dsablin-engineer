db0b99ba3e9b60fa2513faa5ede4637e69db91dd
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2023-11-13 19:13:47 +0200

    added languages buttons

--- src/components/ui/Hero/Hero.tsx
+++ src/components/ui/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import panorama from '../../../assets/img/panorama-black-white.png';
+import Lang from '../../common/Lang/Lang';
 import './Hero.scss';
 
 const Hero = () => {
   return (
     <section className="hero">
+      <Lang />
       <div className="hero__introduce">
         <h1 className="hero__introduce-title">Hi, I am Dmytro Sublin</h1>
         <h2 className="hero__introduce-subtitle">a construction engineer</h2>
